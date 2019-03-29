74ac88903814943cf6af4c7fed0faa27ef7f37ca
Justin Konecny <konecnyjustin@gmail.com>
2019-03-29 09:37:56 -0400

    Add introduction

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import profile from '../resources/profile.jpeg';
 import './Home.css';
-import beach from '../resources/beach.jpeg';
 
 class Home extends Component {
     render() {
         return (
             <div>
-                {/*<div className="container">
-                    <img src={beach} className="Home-image" alt="logo" />
-                    <h1 className="Image-text">
-                        Hi, I'm <span style={{ color: '#303030' }}>Justin</span>, and this is my website.
-                    </h1>
-                </div>*/}
-
-                <Link to="/login" activeClassName="active">
-                    <button className="Login-button">
-                        Login
-                    </button>
-                </Link>
+                <div className="introduction">
+                    <div className="introduction-text">
+                        <h2>Hi, I'm</h2>
+                        <h1>Justin Konecny.</h1>
+                        {/*<p style={{ 'color': '#828282', 'marginLeft': 0 }}>I'm a <span style={{ 'fontWeight': '700' }}>Cybersecurity</span> major at*/}
+                            {/*<span style={{ 'fontWeight': '700' }}> Northeastern University</span>, currently pursuing a*/}
+                            {/*career relating to <span style={{ 'fontWeight': '700' }}>software engineering</span>.</p>*/}
+                    </div>
+                    <div className="introduction-profile">
+                        <img src={ profile } className="profile-image" alt="logo" />
+                    </div>
+                </div>
+                {/*<div className="body">*/}
+                    {/*<div className="about-me">*/}
+                        {/*<h3>About Me</h3>*/}
+                        {/*<h4>Education</h4>*/}
+                        {/*<p>Northeastern University (Boston, MA)</p>*/}
+                        {/*<p>BS Cybersecurity w/ Concentration Cyber Operations</p>*/}
+                    {/*</div>*/}
+                {/*</div>*/}
             </div>
         );
     }
