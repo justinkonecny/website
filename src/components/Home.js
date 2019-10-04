ae9db72d6aab199d937ac824977d4c8404fb4e14
Justin Konecny <konecnyjustin@gmail.com>
2019-10-03 21:40:14 -0400

    Fix navbar bug

--- src/components/Home.js
+++ src/components/Home.js
@@ -87,8 +87,8 @@ class Home extends Component {
     getNavLinksMain() {
         // The links passed to the navigation bar component for rendering (on the right)
         return ([
-            <a className={'nav-link main-link main-link-mobile'} href={resume} target={'_blank'} rel={'noopener noreferrer'} key={'navResume'}>
-                <div className={'nav-inner nav-inner-main nav-inner-mobile'}>Resume</div>
+            <a className={this.state.isMobile ? 'nav-link main-link main-link-mobile' : 'nav-link main-link'} href={resume} target={'_blank'} rel={'noopener noreferrer'} key={'navResume'}>
+                <div className={this.state.isMobile ? 'nav-inner nav-inner-main nav-inner-mobile' : 'nav-inner nav-inner-main'}>Resume</div>
             </a>
         ]);
     }
